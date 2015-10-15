7595b6f73b9824b3ba0a0854761205279f290c20
Fabian Schlager <fabian.schlager@skyr.at>
2015-10-15 18:14:02 +0200

    pyro4-ns does not need to be restarted every time

--- control/run_control.sh
+++ control/run_control.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "Starting persistent Pyro4 nameserver ..."
-pyro4-ns -n 0.0.0.0 -s sql:ns.db &
+if [ "$(pidof process_name)" ]
+then
+    echo "Nameserver already running ..."
+else
+    pyro4-ns -n 0.0.0.0 -s sql:ns.db &
+fi
 
 echo "Starting Control webserver ..."
 python master.py
-
-kill $!
